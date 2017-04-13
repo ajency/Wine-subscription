4754101933e5c4f520faeedda9382e7a9a744400
Ajaj Rajguru <ajaj@ajency.in>
2017-04-13 09:57:12 +0530

    home banner changed and js updated

--- themes/indigo-wine/custom.js
+++ themes/indigo-wine/custom.js
@@ -4,84 +4,104 @@
 
 jQuery(function(){
 
-jQuery(window).scroll(function(){
-  var sticky = jQuery('.ind-custom-menu'),
-      scroll = jQuery(window).scrollTop();
-      imgSrc = 'img/logo-half.png';
+	// Menu fixed code
 
-  if (scroll >= 100){
-  	sticky.addClass('fixed');
-  	// jQuery('.ind-custom-menu #logo img').attr('src',imgSrc);
-  } 
-  else{
-	sticky.removeClass('fixed');
-  }
-  
-});
+	jQuery(window).scroll(function(){
+	  var sticky = jQuery('.ind-custom-menu'),
+	      scroll = jQuery(window).scrollTop();
+	      imgSrc = 'img/logo-half.png';
 
-if(jQuery('.owl-carousel').length){
-    var owl = jQuery('.owl-carousel').data('owlCarousel');
-	owl.destroy();
-}
-
-function slideCarousel(){
-	var owl = jQuery('.owl-carousel').data('owlCarousel');
-	
-	jQuery('.owl-carousel').owlCarousel({
-     	//Basic Speeds
-	    slideSpeed : 200,
-	    paginationSpeed : 800,
-	 
-	    //Autoplay
-	    autoPlay : false,
-	    goToFirst : true,
-	    goToFirstSpeed : 1000,
-	    items : 1,
-	    autoHeight: true,
-	    itemsDesktop : [1199,1],
-    	itemsDesktopSmall : [980,1],
-	    itemsTablet: [768,1],
-    	itemsMobile : [479,1],
-    	addClassActive : true,
-    	// rewindNav: false,
-	 
-	    // Navigation
-	    navigation : true,
-	    navigationText : ["",""],
-	    pagination : true,
-	    // paginationNumbers: true,
-	    // afterAction: moved	
-	    // afterInit: checkfirstlast
+	  if (scroll >= 100){
+	  	sticky.addClass('fixed');
+	  	// jQuery('.ind-custom-menu #logo img').attr('src',imgSrc);
+	  } 
+	  else{
+		sticky.removeClass('fixed');
+	  }
+	  
 	});
-}
 
-setTimeout(slideCarousel, 1000);
+	// Destroy default init
 
-
-
-
-// Readmore
-
-jQuery('.about-details').readmore({
-   speed: 25,
-   collapsedHeight: 210,
-   moreLink: '<a href="#" class="more">Show More <i class="fa fa-angle-down" aria-hidden="true"></i></a>',
-   lessLink: '<a href="#">Less <i class="fa fa-angle-up" aria-hidden="true"></i></a>'
- });
+	if(jQuery('.owl-carousel').length){
+	    var owl = jQuery('.owl-carousel').data('owlCarousel');
+		owl.destroy();
+	}
 
 
+	function slideCarousel(){
+		var owl = jQuery('.owl-carousel').data('owlCarousel');
+		
+		jQuery('.owl-carousel').owlCarousel({
+	     	//Basic Speeds
+		    slideSpeed : 200,
+		    paginationSpeed : 800,
+		 
+		    //Autoplay
+		    autoPlay : false,
+		    goToFirst : true,
+		    goToFirstSpeed : 1000,
+		    items : 1,
+		    autoHeight: true,
+		    itemsDesktop : [1199,1],
+	    	itemsDesktopSmall : [980,1],
+		    itemsTablet: [768,1],
+	    	itemsMobile : [479,1],
+	    	addClassActive : true,
+	    	// rewindNav: false,
+		 
+		    // Navigation
+		    navigation : true,
+		    navigationText : ["",""],
+		    pagination : true,
+		    // paginationNumbers: true,
+		    // afterAction: moved	
+		    // afterInit: checkfirstlast
+		});
+	}
 
-jQuery('.o-menu').click(function(){
-    jQuery('.cols .bottom,.head-overlay').addClass('active');
-    jQuery('body').addClass('blocked');
-});
-
-jQuery('.m-close').click(function(){
-    jQuery('.cols .bottom,.head-overlay').removeClass('active');
-    jQuery('body').removeClass('blocked');
-});
+	setTimeout(slideCarousel, 1000);
 
 
+
+
+	// Readmore
+
+	jQuery('.about-details').readmore({
+	   speed: 25,
+	   collapsedHeight: 210,
+	   moreLink: '<a href="#" class="more">Show More <i class="fa fa-angle-down" aria-hidden="true"></i></a>',
+	   lessLink: '<a href="#">Less <i class="fa fa-angle-up" aria-hidden="true"></i></a>'
+	 });
+
+	// mobile menu
+
+	jQuery('.o-menu').click(function(){
+	    jQuery('.cols .bottom,.head-overlay').addClass('active');
+	    jQuery('body').addClass('blocked');
+	});
+
+	jQuery('.m-close').click(function(){
+	    jQuery('.cols .bottom,.head-overlay').removeClass('active');
+	    jQuery('body').removeClass('blocked');
+	});
+
+	// Menu click scroll
+
+	jQuery('#menu-left-menu li:nth-child(2n) a').click(function(){
+		event.preventDefault();
+		jQuery(this).addClass('active').parent().siblings().children().removeClass('active');
+	    jQuery('html, body').animate({
+	        scrollTop: jQuery(".about-row").offset().top - 110
+	    }, 2000);
+	});
+	jQuery('#menu-left-menu li:nth-child(3n) a').click(function(){
+		event.preventDefault();
+		jQuery(this).addClass('active').parent().siblings().children().removeClass('active');
+	    jQuery('html, body').animate({
+	        scrollTop: jQuery(".why-indigo-row").offset().top - 110
+	    }, 2000);
+	});
 
 });
 
