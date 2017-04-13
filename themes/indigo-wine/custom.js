15404936417fb0ddaecf3b9b151fca691078d4cf
Ajaj Rajguru <ajaj@ajency.in>
2017-04-13 12:58:39 +0530

    navigation scroll and click added

--- themes/indigo-wine/custom.js
+++ themes/indigo-wine/custom.js
@@ -9,11 +9,9 @@ jQuery(function(){
 	jQuery(window).scroll(function(){
 	  var sticky = jQuery('.ind-custom-menu'),
 	      scroll = jQuery(window).scrollTop();
-	      imgSrc = 'img/logo-half.png';
 
 	  if (scroll >= 100){
 	  	sticky.addClass('fixed');
-	  	// jQuery('.ind-custom-menu #logo img').attr('src',imgSrc);
 	  } 
 	  else{
 		sticky.removeClass('fixed');
@@ -55,9 +53,18 @@ jQuery(function(){
 		    navigationText : ["",""],
 		    pagination : true,
 		    // paginationNumbers: true,
-		    // afterAction: moved	
+		    // startDragging: bmoved,
+		    // afterMove: amoved	
 		    // afterInit: checkfirstlast
 		});
+
+		// function bmoved(){
+		// 	jQuery('.banner-row .owl-pagination').addClass('hidden');
+		// }
+		// function amoved(){
+		// 	jQuery('.banner-row .owl-pagination').removeClass('hidden');
+		// }
+
 	}
 
 	setTimeout(slideCarousel, 1000);
@@ -86,22 +93,78 @@ jQuery(function(){
 	    jQuery('body').removeClass('blocked');
 	});
 
-	// Menu click scroll
 
-	jQuery('#menu-left-menu li:nth-child(2n) a').click(function(){
-		event.preventDefault();
-		jQuery(this).addClass('active').parent().siblings().children().removeClass('active');
-	    jQuery('html, body').animate({
-	        scrollTop: jQuery(".about-row").offset().top - 110
-	    }, 2000);
-	});
-	jQuery('#menu-left-menu li:nth-child(3n) a').click(function(){
-		event.preventDefault();
-		jQuery(this).addClass('active').parent().siblings().children().removeClass('active');
-	    jQuery('html, body').animate({
-	        scrollTop: jQuery(".why-indigo-row").offset().top - 110
-	    }, 2000);
-	});
+		// jQuery('#menu-left-menu li a').click(function(){
+		// 	event.preventDefault();
+		// 	var a = jQuery(this).parent().index();
+		// 	jQuery(this).addClass('active').parent().siblings().children().removeClass('active');
+		// 	jQuery('html, body').animate({
+	 //         scrollTop: jQuery('.scroll-'+a).offset().top - 110
+	 //     }, 2000);
+		// });
+	
+		// jQuery(window).scroll(function (event) {
+		//     var scroll = jQuery(window).scrollTop();
+		//     jQuery('#menu-left-menu li').each(function(){
+		//     	var a = jQuery(this).parent().index();
+		// 	    jQuery(this).toggleClass('ok',
+		// 	      scroll >= jQuery('.scroll-'+a).offset().top
+		// 	    );
+		//     });
+		// });
+
+		// jQuery(window).scroll();
+
+
+		// Custom menu click and scroll to particular ID
+
+
+			var topMenu = jQuery(".custom-menu-class"),
+                offset = 40,
+                topMenuHeight = topMenu.outerHeight()+offset,
+                // All list items
+                menuItems =  topMenu.find('a[href*="#"]'),
+                // Anchors corresponding to menu items
+                scrollItems = menuItems.map(function(){
+                  var href = jQuery(this).attr("href"),
+                  id = href.substring(href.indexOf('#')),
+                  item = jQuery(id);
+                  //console.log(item)
+                  if (item.length) { return item; }
+                });
+
+            // so we can get a fancy scroll animation
+            menuItems.click(function(e){
+              var href = jQuery(this).attr("href"),
+                id = href.substring(href.indexOf('#'));
+                  offsetTop = href === "#" ? 0 : jQuery(id).offset().top-topMenuHeight+1;
+              jQuery('html, body').stop().animate({ 
+                  scrollTop: offsetTop
+              }, 1000);
+              e.preventDefault();
+            });
+
+            // Bind to scroll
+            jQuery(window).scroll(function(){
+               // Get container scroll position
+               var fromTop = jQuery(this).scrollTop()+topMenuHeight;
+
+               // Get id of current scroll item
+               var cur = scrollItems.map(function(){
+                 if (jQuery(this).offset().top < fromTop)
+                   return this;
+               });
+
+               // Get the id of the current element
+               cur = cur[cur.length-1];
+               var id = cur && cur.length ? cur[0].id : "";               
+               
+               menuItems.removeClass("active");
+               if(id){
+                    menuItems.parent().end().filter("[href*='#"+id+"']").addClass("active");
+               }
+               
+            });
 
 });
 
@@ -114,11 +177,11 @@ jQuery(document).ready(function() {
 jQuery("#toggleButton").click( function(event){
      event.preventDefault();
      if (jQuery(this).hasClass("isDown") ) {
-     // $( ".navbar-fixed-top" ).animate({ "margin-top": "-62px" }, "fast" );
+     // jQuery( ".navbar-fixed-top" ).animate({ "margin-top": "-62px" }, "fast" );
      jQuery( ".hb-sidebar" ).animate({ "left": "0px" }, "fast" );
      jQuery(this).removeClass("isDown");
      } else {
-     // $( ".navbar-fixed-top" ).animate({ "margin-top": "0px" }, "fast" );
+     // jQuery( ".navbar-fixed-top" ).animate({ "margin-top": "0px" }, "fast" );
      jQuery( ".hb-sidebar" ).animate({ "left": "-780px" }, "fast" );
      jQuery(this).addClass("isDown");
      }
