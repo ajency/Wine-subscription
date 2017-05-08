9df7c73ea7cd045e330a9aafa1f95628819f653c
sairaj <sairaj@ajency.in>
2017-05-08 14:36:48 +0530

    mandatory fields added

--- themes/HighendWP/subscription/js/addproduct.js
+++ themes/HighendWP/subscription/js/addproduct.js
@@ -24,15 +24,40 @@ jQuery( function( $ ) {
       }
    });  
    
-   function editproductdiscount(){
     if($('#_sale_discount_price').val()!='' && $('#_sale_discount_price').val() > 0){
        $('#_sale_discount_percentage').attr('disabled', 'disabled');
     }
     else if($('#_sale_discount_percentage').val()!='' && $('#_sale_discount_percentage').val() > 0){
        $('#_sale_discount_price').attr('disabled', 'disabled');
     }
-   }
+    
+    $('#publish').click(function(){
+    
+      var testervar = $('[id^=\"titlediv\"]')
+      .find('#title');
+      if (testervar.val().length < 1)
+      {
+        $('[id^=\"titlediv\"]').css('background', '#F96');
+        setTimeout($('#ajax-loading').css('visibility', 'hidden'), 100);
+        alert('Product Name is required');
+        setTimeout($('#publish').removeClass('button-primary-disabled'), 100);
+        return false;
+      }
 
-   editproductdiscount();
+      if($('#_regular_price').val() <= 0 || $('#_regular_price').val() == ''){
+        alert("Please Enter Regular Price");
+        return false;
+      }
+      if($('#_sale_price').val()<= 0 || $('#_sale_price').val() == ''){
+        alert("Please Enter Sales Price");
+        return false;
+      }
+      if($('#taxonomy-product_cat input:checked').length==0){
+        alert("Please select a category");
+        return false;
+      }
+  	
+  });
+
 
 });
