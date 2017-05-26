c836c1c99a3f7d85a60dcdc23a3408c262f07ed4
Ajaj Rajguru <ajaj@ajency.in>
2017-05-26 11:46:19 +0530

    #85 - woocommerce signout link to take to homepage.

--- themes/indigo-wine/functions.php
+++ themes/indigo-wine/functions.php
@@ -808,3 +808,22 @@ function wpse_lost_password_redirect() {
 add_action('login_headerurl', 'wpse_lost_password_redirect');
 
 
+// redirects for login / logout
+
+add_filter('woocommerce_login_redirect', 'login_redirect');
+
+function login_redirect($redirect_to) {
+
+    return home_url();
+
+}
+
+add_action('wp_logout','logout_redirect');
+
+function logout_redirect(){
+
+    wp_redirect( home_url() );
+    
+    exit;
+
+}
