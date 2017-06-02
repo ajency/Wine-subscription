9b6893c6a3695db6e68585f9cb30e57453da0ff4
Ajaj Rajguru <ajaj@ajency.in>
2017-06-02 12:53:41 +0530

    how it works section added for empty cart condition

--- themes/HighendWP/woocommerce/cart/cart-empty.php
+++ themes/HighendWP/woocommerce/cart/cart-empty.php
@@ -18,4 +18,29 @@ wc_print_notices();
 <?php do_action( 'woocommerce_cart_is_empty' ); ?>
 
 <p class="aligncenter"><a class="hb-button hb-small-button hb-third-dark" href="<?php echo apply_filters( 'woocommerce_return_to_shop_redirect', get_permalink( wc_get_page_id( 'shop' ) ) ); ?>" target="_self"><?php _e( 'Return To Shop', 'woocommerce' ) ?></a></p>
-<br>
+
+
+<!-- How it works -->
+
+<div class="h-i-w empty">
+	<h1 class="title">How it works</h1>
+	<div class="flex-cols">
+		<div class="cols">
+			<span class="c-icon cal"></span>
+			<p class="content">Hi Your order meets our requirements to opt for a subscription. Would you like to covert this order into a subscription plan?</p>
+		</div>
+		<div class="cols">
+			<span class="c-icon sub"></span>
+			<p class="content">Hi Your order meets our requirements to opt for a subscription. Would you like to covert this order into a subscription plan?</p>
+		</div>
+		<div class="cols">
+			<span class="c-icon pay"></span>
+			<p class="content">Hi Your order meets our requirements to opt for a subscription. Would you like to covert this order into a subscription plan?</p>
+		</div>
+		<div class="cols">
+			<span class="c-icon done"></span>
+			<p class="content">Hi Your order meets our requirements to opt for a subscription. Would you like to covert this order into a subscription plan?</p>
+		</div>
+	</div>
+
+</div>
