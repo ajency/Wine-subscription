2b9e72af1560902685a16471275c166624bf9df9
Ajaj Rajguru <ajaj@ajency.in>
2017-06-09 12:25:01 +0530

    first name passed in menu

--- themes/HighendWP/includes/header-navigation.php
+++ themes/HighendWP/includes/header-navigation.php
@@ -156,11 +156,11 @@
                             }
 
                             $user=wp_get_current_user();
-                            $display_name=$user->display_name;
+                            $display_name=$user->user_firstname;
                            
                            echo '<ul><li class="user-detail"><div class="user"><i class="fa fa-user" aria-hidden="true"></i><i class="fa fa-angle-down arrow-down" aria-hidden="true"></i><div class="drop sub-menu">
                            <ul>
-                           <li class="ind-username">'.$display_name.'</li>
+                           <li class="ind-username">Welcome '.$display_name.'</li>
                            <li><a href="'.$myaccount_page_url.'orders"><i class="fa fa-user" aria-hidden="true"></i> My Account</a></li>
                            <li><a href="'.wp_logout_url( home_url() ).'"><i class="fa fa-power-off" aria-hidden="true"></i> Logout</a></li>
                             </ul></div></div></li></ul>';
