ab6505de9fa26ce0f3ae9099a5e93abf05f2cfa4
Ajaj Rajguru <ajaj@ajency.in>
2017-06-09 18:16:48 +0530

    dropdown added for subscription selection

--- themes/HighendWP/woocommerce/cart/cart.php
+++ themes/HighendWP/woocommerce/cart/cart.php
@@ -212,12 +212,17 @@ do_action( 'woocommerce_before_cart' );
 		</div>
 		<div>
 			<p class="type-title">Type of Subscription</p>
-			<div class="switch">
+			<!-- <div class="switch">
 				<input id="monthly" class="switch-input" checked="checked" name="sub-type" type="radio" value="monthly" autocomplete="off" />
 				<label class="switch-label switch-label-off" for="monthly">Monthly</label>
 				<input id="quarterly" class="switch-input" name="sub-type" type="radio" value="quarterly" autocomplete="off" /><label class="switch-label switch-label-on" for="quarterly">Quarterly</label>
 				<div class="switch-selection"></div>
-			</div>
+			</div> -->
+			<select class="sub-select">
+				<option>Monthly</option>
+				<option>Bimonthly</option>
+				<option>Quarterly</option>
+			</select>
 		</div>
 	</div>
 </div>
