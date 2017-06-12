a8baf1292c326581129033481133c87f699bb62e
Ajaj Rajguru <ajaj@ajency.in>
2017-06-12 11:40:24 +0530

    subscription data hidden by default

--- themes/HighendWP/woocommerce/cart/cart-totals.php
+++ themes/HighendWP/woocommerce/cart/cart-totals.php
@@ -39,7 +39,7 @@ global $woocommerce;
 					<span class="cart-total-value"><?php echo $woocommerce->cart->get_cart_subtotal(); ?></span>
 				</li>
 
-				<li class="clearfix subscribe-data">
+				<li class="clearfix subscribe-data hidden">
 					<span class="cart-total-title">Subscription type <br> Start Date</span>
 					<span class="cart-total-value subscribe-val">Monthly <br><?php echo date('M j, Y'); ?></span>
 				</li>
