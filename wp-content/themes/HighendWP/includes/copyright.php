5e6fc04b10ed325bf7bafd9456990e79b81789cc
Ajaj Rajguru <ajaj@ajency.in>
2017-09-13 10:26:45 +0530

    footer link hidden and author email added

--- wp-content/themes/HighendWP/includes/copyright.php
+++ wp-content/themes/HighendWP/includes/copyright.php
@@ -11,9 +11,14 @@
     <div class="container">
 
         <!-- BEGIN #copyright-text -->
-        <div id="copyright-text">
-            <p><?php echo do_shortcode(hb_options('hb_copyright_line_text')); ?>
-
+        <div id="copyright-text" class="copyright-footer">
+            <p class="copy-text"><?php echo do_shortcode(hb_options('hb_copyright_line_text')); ?>
+            <p class="footer-content__links">
+                <a href="#" class="hidden">Terms &amp; Services</a>
+                <span class="hidden">|</span>
+                <a href="" class="hidden">Privacy Policy</a>
+                <a href="https://ajency.in/" class="visible">Ajency.in</a>
+            </p>
             <!--?php
                 if (hb_options('hb_enable_backlink')){
                     echo ' <a href="http://www.mojomarketplace.com/store/hb-themes?r=hb-themes&utm_source=hb-themes&utm_medium=textlink&utm_campaign=themesinthewild">Theme by HB-Themes.</a>';
