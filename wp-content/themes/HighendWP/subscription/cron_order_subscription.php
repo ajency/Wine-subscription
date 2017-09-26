7552c3bcd4283232c109e659a9f22ad79bad6aa4
sairaj <sairaj@ajency.in>
2017-09-26 17:14:53 +0530

    commented the cron function

--- wp-content/themes/HighendWP/subscription/cron_order_subscription.php
+++ wp-content/themes/HighendWP/subscription/cron_order_subscription.php
@@ -7,8 +7,8 @@ cron Process-Order Subscription Cron
 Template Name: Order Subscription Cron
 */
 
-  global $wpdb;
+/*  global $wpdb;
   
   echo cron_process_subscription_order();
 
-  echo subscription_order_reminder_mail();
+  echo subscription_order_reminder_mail();*/
