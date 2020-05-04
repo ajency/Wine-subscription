85dbb90e44de538a4a5b5610c333f03319ac4119
saeeshtends <saeesh@ajency.in>
2020-05-04 12:21:40 +0530

    order details cutomer fix

--- wp-content/themes/indigo-wine/woocommerce/order/order-details-customer.php
+++ wp-content/themes/indigo-wine/woocommerce/order/order-details-customer.php
@@ -10,54 +10,81 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
+ * @see 	https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 3.4.4
  */
 
 defined( 'ABSPATH' ) || exit;
 
-$show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_address();
 ?>
+
 <section class="woocommerce-customer-details">
-	<div class="cust-detail"> <h3><?php _e( 'Customer details', 'woocommerce' ); ?></h3>
-	<?php if ( $show_shipping ) : ?>
+	<div class="cust-detail">
+	<h3><?php _e( 'Customer details', 'woocommerce' ); ?></h3>
 
-	<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
-		<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+	<table class="woocommerce-table woocommerce-table--customer-details shop_table customer_details">
 
-	<?php endif; ?>
-	</div>
-	<div class="cust-addr">
-	<h2 class="woocommerce-column__title"><?php esc_html_e( 'Billing address', 'woocommerce' ); ?></h2>
-
-	<address>
-		<?php echo wp_kses_post( $order->get_formatted_billing_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
-
-		<?php if ( $order->get_billing_phone() ) : ?>
-			<p class="woocommerce-customer-details--phone"><?php echo esc_html( $order->get_billing_phone() ); ?></p>
+		<?php if ( $order->get_customer_note() ) : ?>
+			<tr>
+				<th><?php _e( 'Note:', 'woocommerce' ); ?></th>
+				<td><?php echo wptexturize( $order->get_customer_note() ); ?></td>
+			</tr>
 		<?php endif; ?>
 
 		<?php if ( $order->get_billing_email() ) : ?>
-			<p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
+			<tr>
+				<th><?php _e( 'Email:', 'woocommerce' ); ?></th>
+				<td><?php echo esc_html( $order->get_billing_email() ); ?></td>
+			</tr>
 		<?php endif; ?>
-	</address>
 
-	<?php if ( $show_shipping ) : ?>
+		<?php if ( $order->get_billing_phone() ) : ?>
+			<tr>
+				<th><?php _e( 'Phone:', 'woocommerce' ); ?></th>
+				<td><?php echo esc_html( $order->get_billing_phone() ); ?></td>
+			</tr>
+		<?php endif; ?>
+
+		<?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
+
+	</table>
+
+	</div>
+
+	<div class="cust-addr">
+
+	<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
+
+	<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
+
+		<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+
+			<?php endif; ?>
+
+			<h3 class="woocommerce-column__title"><?php _e( 'Billing address', 'woocommerce' ); ?></h3>
+
+			<address>
+				<?php echo ( $address = $order->get_formatted_billing_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
+			</address>
+
+			<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
 
 		</div><!-- /.col-1 -->
 
 		<div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
-			<h2 class="woocommerce-column__title"><?php esc_html_e( 'Shipping address', 'woocommerce' ); ?></h2>
+
+			<h3 class="woocommerce-column__title"><?php _e( 'Shipping address', 'woocommerce' ); ?></h3>
+
 			<address>
-				<?php echo wp_kses_post( $order->get_formatted_shipping_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
+				<?php echo ( $address = $order->get_formatted_shipping_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
 			</address>
+
 		</div><!-- /.col-2 -->
 
 	</section><!-- /.col2-set -->
 
 	<?php endif; ?>
 	</div>
-	<?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
-
 </section>
