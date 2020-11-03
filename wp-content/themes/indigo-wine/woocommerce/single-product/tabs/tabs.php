c77351f86df19967a3aebae7a494d527e50b9a27
saeeshtends <saeesh@ajency.in>
2020-11-03 18:57:48 +0530

    show wine pack product

--- wp-content/themes/indigo-wine/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/indigo-wine/woocommerce/single-product/tabs/tabs.php
@@ -27,27 +27,39 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see woocommerce_default_product_tabs()
  */
 $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
-echo json_encode($product_tabs);
+//echo json_encode($product_tabs);
+$show_wine_pack_product = true;
+$wine_pack_prodcut_key = 'wine-pack-product';
 if ( ! empty( $product_tabs ) ) : ?>
 
 	<div class="woocommerce-tabs wc-tabs-wrapper">
 		<ul class="tabs wc-tabs">
 			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+				<?php if($key != $wine_pack_prodcut_key): ?>
 				<li class="<?php echo esc_attr( $key ); ?>_tab">
 					<a href="#tab-<?php echo esc_attr( $key ); ?>">
 						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
 					</a>
 				</li>
+			<?php endif; ?>
 			<?php endforeach; ?>
 		</ul>
 		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+			<?php if($key != $wine_pack_prodcut_key): ?>
 			<div class="panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
 				<?php
 				if ( isset( $product_tab['callback'] ) ) {
 					call_user_func( $product_tab['callback'], $key, $product_tab );
 				}
+				if($show_wine_pack_product){
+					if ( isset( $product_tab['callback'] ) ) {
+						call_user_func( $product_tabs[$wine_pack_prodcut_key]['callback'], $key, $product_tabs[$wine_pack_prodcut_key] );
+					}
+					$show_wine_pack_product = false;
+				}
 				?>
 			</div>
+			<?php endif; ?>
 		<?php endforeach; ?>
 
 		<?php do_action( 'woocommerce_product_after_tabs' ); ?>
