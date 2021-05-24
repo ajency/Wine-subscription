854cad449a65f6bc3fa7eca4e99f28b444fe56a0
Ankita <ankita@ajency.in>
2021-05-24 08:54:29 +0530

    added functionality for taxonomy

--- wp-content/themes/indigo-wine/woocommerce/single-product/meta.php
+++ wp-content/themes/indigo-wine/woocommerce/single-product/meta.php
@@ -13,6 +13,7 @@ global $post, $product;
 
 $cat_count = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
 $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
+
 ?>
 <div class="clear"></div>
 <div class="hb-separator"></div>
@@ -22,6 +23,42 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php echo wc_get_product_category_list( $product->get_id(), ', ', '<span class="posted_in meta-div">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' ', '</span>' ); ?>
 	<?php echo wc_get_product_tag_list( $product->get_id(), '', '<span class="tagged_as meta-div">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . ' ', '</span>' ); ?>
+
+
+	<div class="classification_selected meta-div">
+		<p class="meta-div mb-2">Classifications</p>
+		<?php 
+
+			$taxonomy = 'Classification';
+			$tax_terms = get_terms([
+			    'taxonomy' => $taxonomy,
+			    'hide_empty' => false,
+			]);
+		?>
+		<ul class="classification_block">
+			<?php
+			if (!empty($tax_terms) && ! is_wp_error( $tax_terms )) {
+				foreach ( $tax_terms as $tax_term ) {
+					$class = has_term( $tax_term->term_id, $tax_term->taxonomy ) ? 'selected' : ''; 
+				    echo '<li class="' . $class . '">';
+				    echo '<a href="' . esc_attr( get_term_link( $tax_term, $taxonomy ) ) . 
+				        '" title="' . sprintf( __( "View all products" ), $tax_term->name ) . 
+				        '" ' . '>';
+
+				    echo '<img class="classification_img" src="' . get_field( 'image', $tax_term ) . '">';
+				  
+				    // $tax_terms_name = get_term_by('name', 'Awards' , $taxonomy );
+				    if ( $tax_term->name == 'Awards') {
+			     		echo '<p>' . get_field( 'award_count' ) . '</p>';
+		     		}
+				    echo '<p class="classification_name">'. $tax_term->name .'</p>';
+				    echo '</a>';
+				    echo '</li>';
+				}
+			}
+			?>
+		</ul>
+	</div>
 
 	<div class="bottom-meta-section hb-woo-meta">
 	<?php if ( hb_options('hb_woo_enable_likes') ){ ?>
